fb473e23c59f482e83a189f731a0bc4358a12667
stuckata <stu.kirkov@gmail.com>
2015-01-07 23:59:57 +0200

    refactoring and some minor bugs fixed

--- js/app.js
+++ js/app.js
@@ -6,5 +6,8 @@ app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl: 'templates/home.html',
 		controller: 'HomeCtrl'
-	})
+	});
+	$routeProvider.otherwise({
+		redirectTo: '/'
+	});
 }]);
