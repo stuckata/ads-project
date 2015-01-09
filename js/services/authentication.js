37907cd0a25cf190d872b87212091b1db1d06b82
stuckata <stu.kirkov@gmail.com>
2015-01-09 23:06:45 +0200

    register, login and logout user ready

--- js/services/authentication.js
+++ js/services/authentication.js
@@ -2,16 +2,39 @@ app.factory('authentication', function() {
 
 	var key = 'user';
 
-	function saveUserData (data) {
-		localStorage.setItem(key, data);
+	function saveUserData(data) {
+		localStorage.setItem(key, angular.toJson(data));
 	}
 
-	function getUserData () {
-		localStorage.getItem(key);
+	function getUserData() {
+		return angular.fromJson(localStorage.getItem(key));
+	}
+
+	function getHeaders(argument) {
+		var headers = {};
+		var userData = getUserData();
+
+		if (userData) {
+			headers.Authorization = 'Bearer ' + getUserData().access_token;
+		}		
+
+		return headers;
+	}
+
+	function removeUser() {
+		localStorage.removeItem(key);
+	}
+
+	function isAdmin() {
+		var isAdmin = getUserData().isAdmin;
+		return isAdmin;
 	}
 
 	return {
 		saveUser: saveUserData,
-		getUser: getUserData
+		getUser: getUserData,
+		getHeaders: getHeaders,
+		removeUser: removeUser,
+		isAdmin: isAdmin
 	}
 })
--- js/services/data/userData.js
+++ js/services/data/userData.js
@@ -6,16 +6,26 @@ function($resource, baseServiceUrl, authentication) {
 			.save(user)
 			.$promise
 			.then(function (data) {
-				authentication.saveUser(angular.fromJson(data));
+				authentication.saveUser(data);
 			});
 	}
 
 	function loginUser(user) {
-
+		return $resource(baseServiceUrl + 'user/login')
+			.save(user)
+			.$promise
+			.then(function (data) {
+				authentication.saveUser(data);
+			});
 	}
 
 	function logoutUser() {
-
+		return $resource(baseServiceUrl + 'user/logout')
+			.save(user)
+			.$promise
+			.then(function (data) {
+				authentication.removeUser();
+			});
 	}
 
 	return {
