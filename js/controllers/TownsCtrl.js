852cab9632297783826edd2670b88be4268e5bf3
stuckata <stu.kirkov@gmail.com>
2015-01-10 12:26:00 +0200

    public ads get all data from server, date format set

--- js/controllers/TownsCtrl.js
+++ js/controllers/TownsCtrl.js
@@ -1,4 +1,8 @@
 app.controller('TownsCtrl', ['$scope', 'townsData', function($scope, townsData){
 	
-	$scope.towns = townsData.getTowns();
+	townsData.getTowns()
+		.$promise
+		.then(function (data) {
+			$scope.towns = data;
+		});
 }]);
