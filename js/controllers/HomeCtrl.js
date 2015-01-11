da21db01c50589c47727a6eede2892236a18d33b
stuckata <stu.kirkov@gmail.com>
2015-01-11 13:15:51 +0200

    logout

--- js/controllers/HomeCtrl.js
+++ js/controllers/HomeCtrl.js
@@ -3,4 +3,9 @@ app.controller('HomeCtrl', ['$scope', 'authentication', function($scope, authent
 	$scope.pageTitle = 'Home';
 	$scope.isLoggedIn = authentication.isLoggedIn();
 	$scope.username = authentication.getUsername();
+
+	$scope.logout = function () {
+
+		authentication.removeUser();
+	};
 }]);
