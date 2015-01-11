776f197b4f96892e7edb7f485c8309ee55520120
stuckata <stu.kirkov@gmail.com>
2015-01-11 10:17:44 +0200

    toggleTowns function ready

--- js/controllers/TownsCtrl.js
+++ js/controllers/TownsCtrl.js
@@ -1,10 +1,20 @@
 app.controller('TownsCtrl', ['$scope', '$rootScope', 'townsData', 'filter', function($scope, $rootScope, townsData, filter){
 	
+	$scope.limitTowns = 5;
+
 	townsData.getTowns()
 		.$promise
 		.then(function (data) {
 			$scope.towns = data;
 		});
+
+	$scope.toggleTowns = function (townsData) {
+		if($scope.limitTowns == 5){
+			$scope.limitTowns = $scope.towns.length;
+		} else {
+			$scope.limitTowns = 5;
+		}
+	}
 
 	$scope.townClicked = function (town) {
 		
